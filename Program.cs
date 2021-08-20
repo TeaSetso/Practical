c226823bee80e5d5790e5f5b43ad373fba8ebc5b
TeaSetso <89183763+TeaSetso@users.noreply.github.com>
2021-08-20 09:00:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Favourite Soccer Team : ");
+            string Fav = Console.ReadLine();
+        }
+    }
+}
